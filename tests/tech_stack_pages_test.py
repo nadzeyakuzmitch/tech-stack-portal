988e3adb39e07d32753fd95cb4f27b77caf7f6b6
nadzeyakuzmitch <91139114+nadzeyakuzmitch@users.noreply.github.com>
2022-03-28 23:58:42 -0400

    added new content and unit tests (#9)

--- tests/tech_stack_pages_test.py
+++ tests/tech_stack_pages_test.py
@@ -4,10 +4,15 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/git">Git</a>' in response.data
-    assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
-    assert b'<a class="nav-link" href="/flask">Python / Flask</a>' in response.data
-    assert b'<a class="nav-link" href="/cicd">CI / CD</a>' in response.data
+    assert b'<a class="nav-link active" href="/">Home</a>' in response.data
+    assert b'<a class="dropdown-item" href="/git">Git</a>' in response.data
+    assert b'<a class="dropdown-item" href="/docker">Docker</a>' in response.data
+    assert b'<a class="dropdown-item" href="/flask">Python / Flask</a>' in response.data
+    assert b'<a class="dropdown-item" href="/cicd">CI / CD</a>' in response.data
+    assert b'<a class="dropdown-item" href="/pylint">Pylint & Others</a>' in response.data
+    assert b'<a class="dropdown-item" href="/testing">AAA Testing</a>' in response.data
+    assert b'<a class="dropdown-item" href="/oop">OOP</a>' in response.data
+    assert b'<a class="dropdown-item" href="/solid">SOLID</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
@@ -38,6 +43,30 @@ def test_request_cicd(client):
     response = client.get("/cicd")
     assert response.status_code == 200
     assert b"Continuous" in response.data
+
+def test_request_pylint(client):
+    """This makes the pylint page"""
+    response = client.get("/pylint")
+    assert response.status_code == 200
+    assert b"Pylint" in response.data
+
+def test_request_testing(client):
+    """This makes the testing page"""
+    response = client.get("/testing")
+    assert response.status_code == 200
+    assert b"AAA" in response.data
+
+def test_request_oop(client):
+    """This makes the oop page"""
+    response = client.get("/oop")
+    assert response.status_code == 200
+    assert b"OOP" in response.data
+
+def test_request_solid(client):
+    """This makes the solid page"""
+    response = client.get("/solid")
+    assert response.status_code == 200
+    assert b"SOLID" in response.data
 
 def test_request_page_not_found(client):
     """This makes the not found page"""
